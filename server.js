95ead1052b3e561cea252d80b25c46081750d633
Mark Hansen <mark@markhansen.co.nz>
2010-05-05 16:32:44 +1200

    use prototype to chain settings to default settings: awesome!

--- server.js
+++ server.js
@@ -6,25 +6,16 @@ var fs = require('fs'),
 antinode = require('./lib/antinode');
 
 fs.readFile('./settings.json', function(err, data) {
-    var settings, custom_settings;
+    var settings = {};
     if (err) {
         sys.puts('No settings.json found. Using default settings');
-        antinode.start(antinode.default_settings);
-        process.exit(0);
     }
     try {
-        custom_settings = JSON.parse(data);
+        settings = JSON.parse(data);
     } catch (e) {
         sys.puts('Error parsing settings.json');
         process.exit(1);
     }
-    settings = mixin(antinode.default_settings, custom_settings);
+    settings.__proto__ = antinode.default_settings;
     antinode.start(settings);
 });
-
-function mixin(base, extension) {
-    var out = {};
-    for (var key in base) out[key] = base[key];
-    for (var key in extension) out[key] = extension[key];
-    return out;
-}
